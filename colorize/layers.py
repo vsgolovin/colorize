726d6736346a088e3f123a12cd08d470ca8027ff
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-06-16 17:02:19 +0300

    Make bias optional

--- colorize/layers.py
+++ colorize/layers.py
@@ -68,13 +68,16 @@ class ResidualUnit33(ResidualUnitBase):
     """
     def __init__(self, channels: tuple[int, int, int], bn: bool = True,
                  bn_first: bool = True, pre_activation: bool = False,
-                 identity_conv: bool = False, kaiming_init: bool = True):
+                 identity_conv: bool = False, kaiming_init: bool = True,
+                 conv_bias: bool = False):
         super().__init__(channels, bn, bn_first, pre_activation, identity_conv)
 
         # residual function
-        rf_modules = [nn.Conv2d(channels[0], channels[1], 3, 1, 1, bias=False),
+        rf_modules = [nn.Conv2d(channels[0], channels[1], 3, 1, 1,
+                                bias=conv_bias),
                       self._activation(channels[1]),
-                      nn.Conv2d(channels[1], channels[2], 3, 1, 1, bias=False)]
+                      nn.Conv2d(channels[1], channels[2], 3, 1, 1,
+                                bias=conv_bias)]
         if pre_activation:
             rf_modules = [self._activation(channels[0])] + rf_modules
         elif self.use_bn:
@@ -92,15 +95,19 @@ class ResidualUnit131(ResidualUnitBase):
     """
     def __init__(self, channels: tuple[int, int, int, int], bn: bool = True,
                  bn_first: bool = True, pre_activation: bool = False,
-                 identity_conv: bool = False, kaiming_init: bool = True):
+                 identity_conv: bool = False, kaiming_init: bool = True,
+                 conv_bias: bool = False):
         super().__init__(channels, bn, bn_first, pre_activation, identity_conv)
 
         # residual function
-        rf_modules = [nn.Conv2d(channels[0], channels[1], 1, 1, 0, bias=False),
+        rf_modules = [nn.Conv2d(channels[0], channels[1], 1, 1, 0,
+                                bias=conv_bias),
                       self._activation(channels[1]),
-                      nn.Conv2d(channels[1], channels[2], 3, 1, 1, bias=False),
+                      nn.Conv2d(channels[1], channels[2], 3, 1, 1,
+                                bias=conv_bias),
                       self._activation(channels[2]),
-                      nn.Conv2d(channels[2], channels[3], 1, 1, 0, bias=False)]
+                      nn.Conv2d(channels[2], channels[3], 1, 1, 0,
+                                bias=conv_bias)]
         if pre_activation:
             rf_modules = [self._activation(channels[0])] + rf_modules
         elif self.use_bn:
@@ -114,17 +121,21 @@ class ResidualUnit131(ResidualUnitBase):
 
 class ConvBlock33(ResidualUnit33):
     def __init__(self, channels: tuple[int, int, int], bn: bool = True,
-                 bn_first: bool = True, kaiming_init: bool = True):
+                 bn_first: bool = True, kaiming_init: bool = False,
+                 conv_bias: bool = True):
         super().__init__(channels, bn, bn_first, pre_activation=False,
-                         identity_conv=False, kaiming_init=kaiming_init)
+                         identity_conv=False, kaiming_init=kaiming_init,
+                         conv_bias=conv_bias)
         self.identity = lambda _: 0
 
 
 class ConvBlock131(ResidualUnit131):
     def __init__(self, channels: tuple[int, int, int], bn: bool = True,
-                 bn_first: bool = True, kaiming_init: bool = True):
+                 bn_first: bool = True, kaiming_init: bool = False,
+                 conv_bias: bool = True):
         super().__init__(channels, bn, bn_first, pre_activation=False,
-                         identity_conv=False, kaiming_init=kaiming_init)
+                         identity_conv=False, kaiming_init=kaiming_init,
+                         conv_bias=conv_bias)
         self.identity = lambda _: 0
 
 
